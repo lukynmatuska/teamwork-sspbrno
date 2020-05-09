8a3a32051d6f20b5b077b08c6ada1c31d0fb9b64
Lukáš Matuška <lukynmatuska@gmail.com>
2020-05-09 14:28:54 +0200

    Improve nodejs script for generating source maps

--- generate-source-map.js
+++ generate-source-map.js
@@ -1,15 +1,17 @@
 const generate = require('generate-source-map')
 const fs = require('fs')
 
+const pathToFile = 'static/admin/assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js'
+
 var file = {
-  source: fs.readFileSync('static/js/popper.min.js'),
-  sourceFile: 'popper.min.js'
+  source: fs.readFileSync(pathToFile),
+  sourceFile: pathToFile.split('/')[(pathToFile.split('/').length-1)]
 }
 
 var map = generate(file)
 
 // console.log(map.toString())
-fs.writeFile('static/js/popper.min.js.map', map.toString(), 'utf-8', (err, data) => {
+fs.writeFile(`${pathToFile}.map`, map.toString(), 'utf-8', (err, data) => {
   if (err) {
     return console.error(err)
   }
