69db56a35e889aff32784baf313ba2890678d5b8
Lukáš Matuška <lukynmatuska@gmail.com>
2020-05-11 18:42:16 +0200

    Generate map file for bootstrap in admin section

--- generate-source-map.js
+++ generate-source-map.js
@@ -1,11 +1,11 @@
 const generate = require('generate-source-map')
 const fs = require('fs')
 
-const pathToFile = 'static/admin/assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js'
+const pathToFile = 'static/admin/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 var file = {
   source: fs.readFileSync(pathToFile),
-  sourceFile: pathToFile.split('/')[(pathToFile.split('/').length-1)]
+  sourceFile: pathToFile.split('/')[(pathToFile.split('/').length - 1)]
 }
 
 var map = generate(file)
