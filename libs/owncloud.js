02a4f94504ed032c8e538cdf721aabd7926f2b90
Lukáš Matuška <lukynmatuska@gmail.com>
2021-02-22 21:57:21 +0100

    Improve OwnCloud root folder

--- libs/owncloud.js
+++ libs/owncloud.js
@@ -13,6 +13,19 @@ oc.login(
         .catch(err => {
             return console.error(err)
         })
+    oc.files.mkdir(global.CONFIG.owncloud.folder)
+        .then((status) => {
+            if (status == true) {
+                return console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} OwnCloud folder has been created succesfully`)
+            }
+            console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} OwnCloud: '${status}'`)
+        })
+        .catch((error) => {
+            if (error != 'The resource you tried to create already exists') {
+                return console.error(`${moment().format('YYYY-MM-DD HH:mm:ss')} OwnCloud error: '${error}'`)
+            }
+            console.error(`${moment().format('YYYY-MM-DD HH:mm:ss')} OwnCloud folder already exist`)
+        })
 }).catch(error => {
     return console.error(error)
 })
